376e1923c3faaaa408af1123c09a60930c760262
petr-rusinov <petr-rusinov@yandex.ru>
2022-04-06 20:26:18 +0300

    some functionality

--- CM_Lesson_4/CM_Lesson_4.cpp
+++ CM_Lesson_4/CM_Lesson_4.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void insert_sorted(vector<int>& vec, int num)
+{
+
+}
+
+void task_1()
+{
+	vector<int> sortedVec { 1, 2, 3, 4, 5, 6 };
+	insert_sorted(sortedVec, 4);
+	ostream_iterator<int> osIt{ cout, " " };
+	copy(sortedVec.begin(), sortedVec.end(), osIt);
+}
+
+
 
 int main()
 {
-
+	task_1();
 }
 
